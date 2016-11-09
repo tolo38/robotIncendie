4012abc6bad42e7e987cd56350b072d3a5d83878
moreaale <alexiane.moreau@phelma.grenoble-inp.org>
2016-11-09 16:52:01 +0100

    fichier test scénario 0

--- src/test/TestScenario0.java
+++ src/test/TestScenario0.java
@@ -14,17 +14,16 @@ import general.evenements.RemplirReservoir;
  *
  * @author 66
  */
-public class TestRobots {
+public class TestScenario0 {
     
     public static void main(String[] args) {
         // crée la fenêtre graphique dans laquelle dessiner
         GUISimulator gui = new GUISimulator(1000, 1000, Color.BLACK);
         DonneesSimulation donneesSimulation = new DonneesSimulation(gui);
         // bourrins pour tester
-        donneesSimulation.addEvenement(new RemplirReservoir(2, donneesSimulation.getRobots().get(0)));
         donneesSimulation.addEvenement(new DeplacerRobot(1, donneesSimulation.getRobots().get(0), Direction.NORD));
         donneesSimulation.addEvenement(new DeplacerRobot(2, donneesSimulation.getRobots().get(0), Direction.NORD));
         donneesSimulation.addEvenement(new DeplacerRobot(3, donneesSimulation.getRobots().get(0), Direction.NORD));
-        donneesSimulation.addEvenement(new DeplacerRobot(1, donneesSimulation.getRobots().get(0), Direction.NORD));
+        donneesSimulation.addEvenement(new DeplacerRobot(4, donneesSimulation.getRobots().get(0), Direction.NORD));
     }
 }
