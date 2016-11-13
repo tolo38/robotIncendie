4b4b6924805dc3421367a752e1a9887b9cb3ed13
loiodict <loiodict>
2016-11-13 20:51:06 +0100

    rapport.tex added

--- src/test/TestChefPompier.java
+++ src/test/TestChefPompier.java
@@ -13,7 +13,10 @@ import general.evenements.InterventionUnitaire;
 import general.evenements.RemplirReservoir;
 import general.robots.AbstractRobot;
 import gui.GUISimulator;
+import io.LecteurDonnees;
 import java.awt.Color;
+import java.io.FileNotFoundException;
+import java.util.zip.DataFormatException;
 
 /**
  *
@@ -23,12 +26,17 @@ import java.awt.Color;
 public class TestChefPompier {
 
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Syntaxe: java TestLecteurDonnees <nomDeFichier>");
+            System.exit(1);
+        }
         // crée la fenêtre graphique dans laquelle dessiner
         GUISimulator gui = new GUISimulator(1000, 1000, Color.GRAY);
-        DonneesSimulation donneesSimulation = new DonneesSimulation(gui,"carteSujet.map");
+        //DonneesSimulation donneesSimulation = new DonneesSimulation(gui,"carteSujet.map");
         //DonneesSimulation donneesSimulation = new DonneesSimulation(gui,"desertOfDeath-20x20.map");
         //DonneesSimulation donneesSimulation = new DonneesSimulation(gui,"mushroomOfHell-20x20.map");
         //DonneesSimulation donneesSimulation = new DonneesSimulation(gui,"spiralOfMadness-50x50.map");
+        DonneesSimulation donneesSimulation = new DonneesSimulation(gui,args[0]);
         new ChefPompier(donneesSimulation);
     }
 }
